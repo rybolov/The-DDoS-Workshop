1d6cd7098f7faf8e63a667599f1968449d56f41b
Michael Smith <rybolov@rybolov.net>
2025-09-18 20:17:47 -0400

    Fixing application loads.

--- VirtualMachines/DDoSAttacker/provision.sh
+++ VirtualMachines/DDoSAttacker/provision.sh
@@ -13,9 +13,10 @@ echo "vagrant:ddos" | chpasswd
 # Update the system
 sudo apt-get update
 sudo apt-get dist-upgrade -y
+sudo apt autoremove
 
 # Install packages
-sudo apt-get install siege wget curl tcpdump fping nmap -y
+sudo apt-get DEBIAN_FRONTEND=noninteractive install siege wget curl tcpdump fping nmap -y
 
 echo
 
--- VirtualMachines/DDoSTarget/provision.sh
+++ VirtualMachines/DDoSTarget/provision.sh
@@ -11,11 +11,14 @@ echo "vagrant:ddos" | chpasswd
 # Update the system
 sudo apt-get update
 sudo apt-get dist-upgrade -y
+sudo apt autoremove
 
-# Install packages.  This should have wireshark but it was failing for me, so I took it out for now.
-sudo apt-get install ubuntu-desktop apache2 nmap -y
+# Install packages.
+sudo DEBIAN_FRONTEND=noninteractive apt install gdm3 ubuntu-desktop apache2 nmap wireshark -y
 
 # Create a simple index.html file
 echo "<h1>Hello from Ubuntu 25.04!</h1>" | sudo tee /var/www/html/index.html
 
-echo "Post-install script finished. Rebooting now."
+/etc/init.d/gdm3 restart
+
+echo "Post-install script finished."
